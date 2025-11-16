386eb256d63e752a17180b247bc135a37ee4fe3e
chaitu426 <interiormern@gmail.com>
2025-11-16 17:48:38 +0530

    git add .

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sarvasvaa.management',
+  appName: 'sarvasvaa',
+  webDir: 'dist'
+};
+
+export default config;
