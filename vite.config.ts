ea9017c294760606fa6d1128992d69d750d42504
chaitu426 <interiormern@gmail.com>
2025-11-19 19:22:27 +0530

    add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -13,6 +14,30 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Sarvasvaa",
+        short_name: "Sarvasvaa",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#317EFB",
+        icons: [
+          {
+            src: "/image.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/image.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      }
+    })
   ].filter(Boolean),
   resolve: {
     alias: {
